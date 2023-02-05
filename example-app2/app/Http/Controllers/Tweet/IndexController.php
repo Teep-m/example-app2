bc72f68594759f0a9e333fb3c175d016bf913a04
Teep-m <103185983+Teep-m@users.noreply.github.com>
2023-02-06 00:43:32 +0900

    fixing problems

--- example-app2/app/Http/Controllers/Tweet/IndexController.php
+++ example-app2/app/Http/Controllers/Tweet/IndexController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Tweet;
-
+use App\Services\TweetService;
 //!①viewヘルパー関数のみを使う場合はview()とする
 //use Illuminate\Support\Facades\View; //!②Facadeを用いて呼び出す場合はView::makeとする
 //use Illuminate\View\Factory; //!③Factoryをインジェクションして呼び出す場合は$factory->makeとする
@@ -14,8 +13,8 @@ class IndexController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        //dd($tweets);
+        $TweetService = new TweetService();
+        $tweets = $TweetService->getTweets();
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
