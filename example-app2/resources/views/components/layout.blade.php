9d24161eff1d86626b06c173dd161765429c15a6
Teep-m <103185983+Teep-m@users.noreply.github.com>
2023-02-14 23:51:48 +0900

    chap3: pages done

--- example-app2/resources/views/components/layout.blade.php
+++ example-app2/resources/views/components/layout.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <title>{{ $title ?? 'Tweet App' }}</title>
+    @stack('css')
 </head>
+
 <body class="bg-gray-50">
     {{ $slot }}
 </body>
+
 </html>
